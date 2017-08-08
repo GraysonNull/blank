fb8e88ea67185bfc653df9f548ce28a79093cd86
Sloane Smith <s10dimensional@gmail.com>
2017-08-08 16:33:38 -0700

    Search tweaks

--- static/js/autocomplete-search.js
+++ static/js/autocomplete-search.js
@@ -1,6 +1,7 @@
 const client = algoliasearch('5PHVQPX4AR', '92575152510e5cdaf7a0df17c446d879');
 const otherIndex = client.initIndex('help_center_other');
 const docsIndex = client.initIndex('help_center_docs');
+const guidesIndex = client.initIndex('help_center_guides');
 
 // Autocomplete search bar
 autocomplete('#autocompletesearch-box', {
@@ -29,6 +30,25 @@ autocomplete('#autocompletesearch-box', {
         empty: "<div class='aa-empty'>No matching files</div>"
     },
     {
+        source: autocomplete.sources.hits(guidesIndex, {
+            hitsPerPage: 5
+        }),
+        displayKey: 'title',
+        name: 'guides',
+        templates: {
+            header: '<h2 class="aa-header">Guides</h2>',
+            suggestion: function(suggestion) {
+                const hasDescription = (suggestion._highlightResult).hasOwnProperty("description");
+
+                return '<h3 class="aa-suggestion-header">' +
+                    suggestion._highlightResult.title.value + '</h3>' +
+                    '<p class="aa-suggestion-description">' + (hasDescription ? suggestion._highlightResult.description.value : "") + '</p>' +
+                    '<span class="icon u-guidesIcon"></span>'
+            }
+        },
+        empty: "<div class='aa-empty'>No matching files</div>"
+    },
+    {
         source: autocomplete.sources.hits(otherIndex, {
             hitsPerPage: 5
         }),
@@ -38,9 +58,10 @@ autocomplete('#autocompletesearch-box', {
             header: '<h2 class="aa-header">Other</h2>',
             suggestion: function(suggestion) {
                 const hasDescription = (suggestion._highlightResult).hasOwnProperty("description");
+                const hasTitle = (suggestion._highlightResult).hasOwnProperty("title");
 
                 return '<h3 class="aa-suggestion-header">' +
-                    suggestion._highlightResult.title.value + '</h3>' +
+                    (hasTitle ? suggestion._highlightResult.title.value : "") + '</h3>' +
                     '<p class="aa-suggestion-description">' + (hasDescription ? suggestion._highlightResult.description.value : "") + '</p>'
             }
         },
